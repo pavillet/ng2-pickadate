748042c017fcc758e439469b19025ddfe2b3f76f
sholzmayer <simon@holzmayer.net>
2016-10-04 19:55:35 +0200

    chore: release version 0.3.20

--- lib/ng2-pickadate.component.d.ts
+++ lib/ng2-pickadate.component.d.ts
@@ -1,8 +1,10 @@
 /// <reference types="pickadate" />
-import { ElementRef, AfterViewInit, OnDestroy, EventEmitter } from '@angular/core';
+import { ElementRef, AfterViewInit, OnDestroy, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { ControlValueAccessor } from '@angular/forms';
+import './shared/picker.date';
 export declare class PickadateDirective implements AfterViewInit, OnDestroy, ControlValueAccessor {
     private el;
+    private cd;
     format: string;
     disable: Pickadate.DateItem[];
     min: Pickadate.MinOrMaxDateOption;
@@ -14,17 +16,18 @@ export declare class PickadateDirective implements AfterViewInit, OnDestroy, Con
     onSelect: EventEmitter<Date>;
     private input;
     private propagateChange;
-    private date;
     private datepicker;
-    constructor(el: ElementRef);
+    private pickerInitialized;
+    onClick(event: any): void;
+    constructor(el: ElementRef, cd: ChangeDetectorRef);
     ngAfterViewInit(): any;
+    ngOnChanges(): void;
     ngOnDestroy(): void;
     writeValue(value: string): void;
     registerOnChange(fn: any): void;
     registerOnTouched(): void;
-    assignValueFromHostInput(): void;
+    private findInputElementInHost();
+    private initializePickadateListeners();
     inputValue: string;
-    private assignGivenInputElement();
-    private findGivenInputElement();
     readonly options: Pickadate.DateOptions;
 }
